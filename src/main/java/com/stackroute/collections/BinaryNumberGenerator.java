6a1d9c2f4769035aa98d75f034de3c5932c201d2
shahid <shahdbest@gmail.com>
2022-08-18 14:56:54 +0530

    a

---
+++ src/main/java/com/stackroute/collections/BinaryNumberGenerator.java
@@ -0,0 +1,8 @@
+package com.stackroute.collections;
+
+public class BinaryNumberGenerator {
+    //write logic to find binary number from 1 to given input
+    public String findBinaryNumbersSequence(int input) {
+        return null;
+    }
+}
